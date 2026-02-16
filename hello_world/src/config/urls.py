0aa5ee03eae63edbc7bfbe6e5352123c67b405b8
Artem Samokrutov <arsamokrutov@gmail.com>
2026-02-17 01:47:38 +0300

    Simplified urls for demo

--- hello_world/src/config/urls.py
+++ hello_world/src/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('lotr/', include('nosql_lotr.urls'))
+    path('', include('nosql_lotr.urls'))
 ]
--- hello_world/src/nosql_lotr/urls.py
+++ hello_world/src/nosql_lotr/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.neo4j_test_view, name='neo4j_test')
+    path('', views.neo4j_test_view, name='index')
 ]
