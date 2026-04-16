86cdc461c0d47c7060d27db2cf2eb3cd19e92fbd
Чинаева Маргарита <144445296+Margarita-Chi@users.noreply.github.com>
2026-04-16 05:38:46 +0300

    Add urls for meta

--- backend/apps/meta/urls.py
+++ backend/apps/meta/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from . import views
 
-
-urlpatterns: list = []
+urlpatterns = [
+    path('node-types/', views.node_types, name='node-types'),
+    path('relation-types/', views.relation_types, name='relation-types'),
+]
